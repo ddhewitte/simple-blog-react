45dcff94a0225b445e095a0666aaedf93aefe2cf
adjihyphen <adji.hendrawan@moneyherogroup.com>
2025-11-24 16:49:57 +0700

    Login page design + premium List

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,28 @@
 export default function Login(){
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submit..")
+    }
+
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
+            <div className="mt-10 mb-10 gap-3 shadow-md p-5">
+                <h1 className="text-2xl">
+                    Membership Login
+                </h1>
+                <div className="flex gap-2 mt-10">
+                    <label typeof="username">Username </label>
+                    <input type="text" id="username" className="border border-gray-700 rounded-md p-2"></input>
+                </div>
+                <div className="flex gap-4 mt-5">
+                    <label typeof="password">Password</label>
+                    <input type="password" id="password" className="border border-gray-700 rounded-md p-2"></input>
+                </div>
+                <div className="flex gap-4 mt-5">
+                    <button type="submit" className="bg-amber-950 text-white p-2 rounded-md cursor-pointer">Login</button>
+                </div>
+            </div>
             
         </form>
     )
--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,3 +1,50 @@
+import { useEffect } from "react"
+import { generalPost } from "../services/post"
+import { useState } from "react"
+import { truncate } from "../utils/helper";
+import { Link } from "react-router-dom";
+
 export default function Premium(){
-    return(<div>Premium Content..</div>)
+
+    const [premiumPost, setPremiumPost] = useState([]);
+
+    useEffect(() => {
+        const getPremiumPost = async () => { 
+            const getPost =  await generalPost();
+
+            const filterGetPost = getPost.filter((item) => item.isExclusive === true);
+            setPremiumPost(filterGetPost);
+        }
+        
+        getPremiumPost();
+    }, []);
+
+    return(
+        <>
+            <div className="flex-row">
+
+                <h1 className="text-2xl font-bold mb-3">Premium Articles</h1>
+
+                { premiumPost && premiumPost.map((data) => (
+
+                    <div className="w-full md:w-150 h-auto border-3 border-gray-200 p-5 rounded-md mb-3" key={data.id}>
+                        <h1 className="text-sm md:text-xl mb-3 font-bold md:font-normal">{data.title}</h1>
+                        <div className="flex justify-between gap-2 mb-3">
+                            <img className="w-25 h-15 md:w-50 md:h-auto rounded-md" src={data.image}></img>
+                            <p className="text-sm md:text-md">{truncate(data.content, 40)}</p>
+                            
+                        </div>
+                        <div className="flex justify-end text-white">
+                            <Link to={`/detail/`+data.id}>
+                            <button className="p-2 bg-black rounded-md cursor-pointer hover:shadow-xl">
+                                Read More
+                            </button>
+                            </Link>
+                        </div>
+                    </div>
+
+                ))}
+            </div>
+        </>
+    )
 }
